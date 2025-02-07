326534e4a6fde9a10d9eee12b046fe4e8f7fdaa6
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-07 22:43:54 +0100

    First commit to the repository

---
+++ src/main/java/com/ouicha_hammou_yassine/pay_my_buddy/PayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ouicha_hammou_yassine.pay_my_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouicha_hammou_yassine/pay_my_buddy/PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouicha_hammou_yassine.pay_my_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
