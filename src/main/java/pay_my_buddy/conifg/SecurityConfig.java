218e547f8b1c353a50f15c565708f176143842a1
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-11 10:31:28 +0100

    Security desabled for now, because of issues of encoding password, for now we authorizz all HTTPs requests

--- src/main/java/pay_my_buddy/conifg/SecurityConfig.java
+++ src/main/java/pay_my_buddy/conifg/SecurityConfig.java
@@ -2,8 +2,10 @@ package pay_my_buddy.conifg;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Autoriser toutes les requêtes et désactiver CSRF
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
     }
 }
