ca4e9629539f1bd0f66ffe465052b9dd7ac67129
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-15 18:39:20 +0100

    Testing of loginPage of authcontroller

---
+++ src/test/java/pay_my_buddy/integration/AuthControllerTest.java
@@ -0,0 +1,43 @@
+package pay_my_buddy.integration;
+
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import pay_my_buddy.PayMyBuddyApplication;
+import pay_my_buddy.service.UserService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = PayMyBuddyApplication.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testLoginPage() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+
+
+}
+
+
+
