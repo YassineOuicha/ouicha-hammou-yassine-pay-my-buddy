4278cb621598a32f9b186a84eff7af2e978922ce
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-15 11:43:12 +0100

    Tests of sent and received transactions

--- src/test/java/pay_my_buddy/unit/TransactionServiceTest.java
+++ src/test/java/pay_my_buddy/unit/TransactionServiceTest.java
@@ -12,11 +12,12 @@ import pay_my_buddy.repository.TransactionRepository;
 import pay_my_buddy.repository.UserRepository;
 import pay_my_buddy.service.TransactionService;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class TransactionServiceTest {
 
@@ -79,6 +80,62 @@ public class TransactionServiceTest {
 
         // Assert
         assertNull(transaction);
+    }
+
+    @Test
+    public void testGetSentTransactions(){
+        // Arrange
+        String email ="Yassine@example.com";
+
+        Transaction tr1 = new Transaction();
+        tr1.setAmount(50.0);
+        tr1.setDescription("Remboursement");
+
+        Transaction tr2 = new Transaction();
+        tr2.setAmount(100.0);
+        tr2.setDescription("Cadeau");
+
+        when(transactionRepository.findTransactionsSentByUser(email))
+                .thenReturn(List.of(tr1, tr2));
+        // Act
+        List<Transaction> sentTransactions = transactionService.getSentTransactions(email);
+
+        // Assert
+        assertNotNull(sentTransactions);
+        assertEquals(2, sentTransactions.size());
+        assertEquals("Remboursement", sentTransactions.get(0).getDescription());
+        assertEquals(50, sentTransactions.get(0).getAmount());
+        assertEquals("Cadeau", sentTransactions.get(1).getDescription());
+        assertEquals(100, sentTransactions.get(1).getAmount());
+        verify(transactionRepository, times(1)).findTransactionsSentByUser(email);
+    }
+
+    @Test
+    public void testGetReceivedTransactions(){
+        // Arrange
+        String email ="Georges@example.com";
+
+        Transaction tr1 = new Transaction();
+        tr1.setAmount(150.0);
+        tr1.setDescription("Virement");
+
+        Transaction tr2 = new Transaction();
+        tr2.setAmount(600.0);
+        tr2.setDescription("Loyer");
+
+        when(transactionRepository.findTransactionsReceivedByUser(email))
+                .thenReturn(List.of(tr1, tr2));
+        // Act
+        List<Transaction> receivedTransactions = transactionService.getReceivedTransactions(email);
+
+        // Assert
+        assertNotNull(receivedTransactions);
+        assertEquals(2, receivedTransactions.size());
+        assertEquals("Virement", receivedTransactions.get(0).getDescription());
+        assertEquals(150, receivedTransactions.get(0).getAmount());
+        assertEquals("Loyer", receivedTransactions.get(1).getDescription());
+        assertEquals(600.0, receivedTransactions.get(1).getAmount());
+        verify(transactionRepository, times(1)).findTransactionsReceivedByUser(email);
     }
 
 }
